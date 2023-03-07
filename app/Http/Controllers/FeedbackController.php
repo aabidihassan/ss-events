b23d6c7a2a50bdf300335d32fca23963194df15f
mqamyouness <mqamyouness18@gmail.com>
2023-03-07 16:56:58 +0000

    nothing change

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -8,14 +8,8 @@ use Illuminate\Http\Request;
 
 class FeedbackController extends Controller
 {
-    public function addCommit($var)
+    public function addCommit(Request $request)
     {
-        /*$feedback = new Feedback();
-        $feedback->id_client = $var->id_client;
-        $feedback->id_fournisseur = $var->id_fournisseur;
-        $feedback->commentText = $var->commentaire;
-        $feedback->rating = $var->rating;
-        $feedback->save();*/
         $request->validate([
             'email' => 'required|email|unique:newsletter_emails,email'
         ]);
