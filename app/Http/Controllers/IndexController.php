ca3f3f86a57cd5da0cd792648d50f0511e740ce7
mqamyouness <mqamyouness18@gmail.com>
2023-03-08 10:03:11 +0000

    aficher avg rating and nombre feedback fournisseur in page index

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\Citie;
 use App\Models\Service;
 use App\Models\Fournisseur;
+use App\Models\Feedback;
+use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
@@ -13,6 +15,11 @@ class IndexController extends Controller
         $cities = Citie::all();
         $services = Service::all();
         $fournisseurs = Fournisseur::where('statut', true)->take(6)->whereNotNull('photo')->get();
-        return view('index', ["cities"=>$cities, "services"=>$services, "fournisseurs"=>$fournisseurs]);
+        $fournisseurIds = $fournisseurs->pluck('id')->toArray();
+        $avgsRatings = Feedback::select('id_fournisseur', DB::raw('ROUND(avg(rating),1) as average'), DB::raw('count(*) as count'))
+                                    ->whereIn('id_fournisseur',$fournisseurIds)
+                                    ->groupBy('id_fournisseur')
+                                    ->get();
+        return view('index', ["cities"=>$cities, "services"=>$services, "fournisseurs"=>$fournisseurs, "avgsRatings" => $avgsRatings]);
     }
 }
